2e26c53dc3d39a4186e1bf52bbc9094f364693de
olve123 <olve.husby@gmail.com>
2016-04-08 22:25:37 +0200

    Experiments and paddle

--- Breakout/Main.cpp
+++ Breakout/Main.cpp
@@ -10,6 +10,23 @@
 
 using namespace std;
 
+//experiment
+class Brick {
+
+};
+
+class Board {
+public:
+	Board(SDL_Renderer* renderer);
+
+
+
+};
+
+
+//--------------------------------------------------------------//
+
+
 //SET FRAMERATE TO 60FPS
 void cap_framerate(Uint32 starting_tick) {
 	if ((1000 / fps) > SDL_GetTicks() - starting_tick) {
@@ -17,14 +34,14 @@ void cap_framerate(Uint32 starting_tick) {
 	}
 }
 
-class Sprite {
+class block {
 private: 
 	SDL_Surface *image;
 	SDL_Rect rect; 
 
 	int origin_x, origin_y;
 public: 
-	Sprite(Uint32 color, int x, int y, int w = 50, int h = 20) {
+	block(Uint32 color, int x, int y, int w = 50, int h = 20) {
 		image = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0);
 		SDL_FillRect(image, NULL, color);
 
@@ -47,12 +64,38 @@ public:
 
 };
 
+class paddle {
+private:
+	SDL_Surface *image;
+	SDL_Rect rect;
 
+	int origin_x, origin_y;
+
+public:
+	paddle(Uint32 color, int x, int y, int w = 200, int h = 20) {
+		image = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0);
+		SDL_FillRect(image, NULL, color);
+
+		rect = image->clip_rect;
+
+		origin_x = rect.w / 2;
+		origin_y = rect.h / 2;
+
+		rect.x = x - origin_x;
+		rect.y = y - origin_y;
+	}
+	void draw(SDL_Surface *destination) {
+		SDL_BlitSurface(image, NULL, destination, &rect);
+	}
+};
+
+
+//////////////////////////////////////////////////////
 int main(int argc, char *argv[]) {
 	//init sdl
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window *window = NULL;
-
+	Uint8 keystate;
 	//window properties
 	window = SDL_CreateWindow(
 		"BREAKOUT",
@@ -75,6 +118,7 @@ int main(int argc, char *argv[]) {
 	Uint32 white = SDL_MapRGB(screen->format, 255, 255, 255);
 	Uint32 black = SDL_MapRGB(screen->format, 0, 0, 0);
 	Uint32 red = SDL_MapRGB(screen->format, 255, 0, 0);
+	Uint32 blue = SDL_MapRGB(screen->format, 0, 0, 255);
 
 	//BACKGROUNDCOLOR
 	SDL_FillRect(screen, NULL, white);
@@ -85,39 +129,48 @@ int main(int argc, char *argv[]) {
 
 	//INIT 
 	Uint32 starting_tick;
-
 	
+
 	//DRAWING RECTANGLES
-	Sprite object1(red, 30, 20);
+	block object1(red, 30, 20);
 	object1.draw(screen);
 
-	Sprite object2(black, 82, 20);
+	block object2(black, 82, 20);
 	object2.draw(screen);
 
+	block object3(red, 134, 20);
+	object3.draw(screen);
 
+	block object4(black, 186, 20);
+	object4.draw(screen);
 
-	//SDL_Rect rect;
-	//rect.x = 10;
-	//rect.y = 10;
-	//rect.w = 50;
-	//rect.h = 20;
-	//SDL_FillRect(screen, &rect, black);
+	block object5(red, 238, 20);
+	object5.draw(screen);
+
+	block object6(black, 290, 20);
+	object6.draw(screen);
+
+	block object7(red, 342, 20);
+	object7.draw(screen);
+
+	paddle paddle(blue, 150, 580);
+	paddle.draw(screen);
 
 	
-	//rect.x = 62;
-	//SDL_FillRect(screen, &rect, red);
-	//UPDATE
-	SDL_UpdateWindowSurface(window);
-
-
 	SDL_Event event;
 	bool running = true;
 
 	//GAMELOOP
 	while (running) {
-
+		
+		//keystate = SDL_GetKeyboardState(NULL);
+		//if (keystate[SDLK_LEFT]) {
+		//	paddle. -= 2;
+		//}
+		SDL_UpdateWindowSurface(window);
 		starting_tick = SDL_GetTicks();
 		while (SDL_PollEvent(&event)) {
+			
 			if (event.type == SDL_QUIT) {
 				running = false;
 				break;
